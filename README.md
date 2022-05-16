00da17674f1af926d72e7cc79a5f307d4b59bc59
xiong-jie-y <fwvillage@gmail.com>
2022-05-16 23:12:00 +0900

    add

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ## Gaze Estimator
 ```
 sh downloader.sh
-python gaze_estimator.py --input ${INPUT_FILE}
-```# g_api_examples
+python gaze_estimator.py --input ${INPUT_FILE} --out output.mp4
+```
---
+++ gaze_estimator/downloader.sh
@@ -0,0 +1,4 @@
+/opt/intel/openvino_2021/deployment_tools/open_model_zoo/tools/downloader/downloader.py --name gaze-estimation-adas-0002 --output_dir .
+/opt/intel/openvino_2021/deployment_tools/open_model_zoo/tools/downloader/downloader.py --name facial-landmarks-35-adas-0002 --output_dir .
+/opt/intel/openvino_2021/deployment_tools/open_model_zoo/tools/downloader/downloader.py --name head-pose-estimation-adas-0001 --output_dir .
+/opt/intel/openvino_2021/deployment_tools/open_model_zoo/tools/downloader/downloader.py --name face-detection-retail-0005 --output_dir .
